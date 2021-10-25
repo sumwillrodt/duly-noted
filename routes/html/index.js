e01785f6528c716ec452510af4c1f696d2ec23ae
swillrodt <sumnerwillrodt@gmail.com>
2021-10-24 19:27:59 -0700

    setup frontend

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,14 +1,16 @@
 const path = require('path');
-const app = require('express').app();
+const router = require('express').Router();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
-const app - express();
+const app = express();
+const htmlRoutes = require('./routes/html/index');
+const apiRoutes = require('./routes/api/notes')
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`API server is on port 3001!`);
+});
