ed05bb4df02b41a9663882480415f8a9193a50da
swillrodt <sumnerwillrodt@gmail.com>
2021-10-24 23:22:50 -0700

    add POST route

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
+const { createNote } = require('../../lib/notes');
 const { notes } = require('../../db/db');
 
 router.get('/db', (req, res) => {
     let results = notes;
     res.json(results);
-});  
+});
+
+router.post('/db', (req, res) => {
+    res.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    
+    res.json(note);
+});
 
 module.exports = router;
